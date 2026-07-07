159048016961ae28c1693ea5f1bf4362e6596a48
Ivan Schmid <office@ivanschmid.com>
2026-07-07 11:07:08 +0200

    chore: add Tailwind integration and build configuration for Hostiger deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+	integrations: [tailwind()],
 	output: 'static',
-	site: 'https://ivanschmid.com'
+	site: 'https://ivanschmid.com',
+	build: {
+		outDir: './dist',
+	}
 });
